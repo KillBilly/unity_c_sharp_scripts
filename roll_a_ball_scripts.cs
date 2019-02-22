28b73d46a77ba556187692f388ce8db0593cbe87
KillBill <KillBilly@users.noreply.github.com>
2019-02-21 23:30:20 -0800

    Update roll_a_ball_scripts.cs

--- roll_a_ball_scripts.cs
+++ roll_a_ball_scripts.cs
@@ -100,3 +100,57 @@ public class PlayerController : MonoBehaviour {
         }
     }
 }
+
+
+// the below codes add Text to player as a message to display when certain conditions happen
+// UnityEngine.UI: Text Class: .text
+using UnityEngine;
+using UnityEngine.UI; // must import this to use Text Class
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ( "Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
