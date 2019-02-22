cd0f4b4136b544ea1af910e2e4b2ded836c8062a
KillBill <KillBilly@users.noreply.github.com>
2019-02-21 22:29:52 -0800

    Update roll_a_ball_scripts.cs

--- roll_a_ball_scripts.cs
+++ roll_a_ball_scripts.cs
@@ -1,3 +1,7 @@
+/* the below codes create a Player Controller that would take users input as force to move */
+// Rigidbody Class: .AddForce()
+// Input Class: .GetAxis()
+// Vector3 struct
 using UnityEngine;
 using System.Collections;
 
@@ -9,7 +13,7 @@ public class PlayerController : MonoBehaviour {
 
     void Start ()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>(); // add physics to the player
     }
 
     void FixedUpdate ()
@@ -22,3 +26,42 @@ public class PlayerController : MonoBehaviour {
         rb.AddForce (movement * speed);
     }
 }
+
+
+// the below code create a Camera Controller that force Camera to move with Player
+// GameObject Class: .transform.position
+// Transform Class: .position
+using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start ()
+    {
+        offset = transform.position - player.transform.position; // camera's position - player's position
+    }
+    
+    void LateUpdate ()
+    {
+        transform.position = player.transform.position + offset; // transform is the position attributes
+    }
+}
+
+
+// the below code create a Rotator to rotate object
+// Transform Class: .Rotate()
+using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    void Update () 
+    {
+        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+    }
+}
+
